0f81a21728443a3f5297e12ae40bbc19c9358b37
Julio Jimenez <julio@julioj.com>
2021-01-29 23:33:31 -0500

    wip

--- cmd/calculate.go
+++ cmd/calculate.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/devops-kung-fu/go-shs/api/v1"
 )
 
 // calculateCmd represents the calculate command
@@ -13,11 +14,20 @@ var (
 		Short: "Calculates the Security Health Score",
 		Long:  `Calculates the Security Health Score`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Ran Calculate Command")
+		
 		},
 	}
 )
 
+// Vector - Single CVSS vector string.
+var Vector string
+
 func init() {
+	rootCmd.Flags().StringVarP(&Vector, "vector", "v", "", "Calculates security health score based on a single vector string.")
 	rootCmd.AddCommand(calculateCmd)
+}
+
+func calculateVector(vector string) int {
+	goSHS := v1.NewAPI()
+	goSHS.Cal
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/djschleen/shs
+module github.com/devops-kung-fu/shs
 
 go 1.15
 
-require github.com/spf13/cobra v1.1.1
+require (
+	github.com/devops-kung-fu/go-shs v0.0.2 // indirect
+	github.com/spf13/cobra v1.1.1
+)
