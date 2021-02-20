fbf2f5cb232d0e8df1e980d7146691296c97a410
Julio Jimenez <julio@julioj.com>
2021-02-20 00:11:01 -0500

    feat: Weight

--- cmd/calculate.go
+++ cmd/calculate.go
@@ -21,10 +21,14 @@ var (
 
 // Vector - Single CVSS vector string.
 var Vector []string
+// Weight - Specify the weight to be used for calculations.
+var Weight float32
 
 func init() {
 	calculateCmd.Flags().StringArrayVarP(&Vector, "vector", "v", []string{}, "Calculates security health score based on a single vector string.")
 	calculateCmd.MarkFlagRequired("vector")
+	calculateCmd.Flags().Float32VarP(&Weight, "weight", "w", 1.05, "Specify the weight to be used in calculating score. Default: 1.05.")
+	calculateCmd.MarkFlagRequired("weight")
 	rootCmd.AddCommand(calculateCmd)
 }
 
